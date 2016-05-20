3612b29a61c92e3fba6d7fc5afff07e407482821
oBlank <dyh1919@gmail.com>
2016-05-20 21:20:33 +0800

    flex align

--- client/components/Loader/index.js
+++ client/components/Loader/index.js
@@ -6,7 +6,13 @@ export default class Loader extends Component {
     render() {
         return (
             <div className={style.container}>
-                <div>
+                <div className={style.pacman_b}>
+                    <div>H</div>
+                    <div>U</div>
+                    <div>N</div>
+                    <div>T</div>
+                </div>
+                
                     <div className={style.pacman}>
                         <div></div>
                         <div></div>
@@ -15,20 +21,13 @@ export default class Loader extends Component {
                         <div>O</div>
                         <div>L</div>
                     </div>
-                </div>
 
 
-                <div className={style.right}>
-                    <div className={style.pacman_b}>
-                        <div>H</div>
-                        <div>U</div>
-                        <div>N</div>
-                        <div>T</div>
-                    </div>
-                </div>
-                <div className={style.hint}>
-                    加载中...
-                </div>
+
+
+
+
+
             </div>
         )
     }
